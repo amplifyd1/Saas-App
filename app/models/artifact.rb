c9556633df26ef6fe77eff2b12e32b1ae27b14e8
wesley sprague <wesleysprague@yahoo.com>
2018-11-19 14:30:38 -0500

    upload method for s3 artifacts, list partial for artifacts

--- app/models/artifact.rb
+++ app/models/artifact.rb
@@ -1,11 +1,23 @@
 class Artifact < ApplicationRecord
+
+  before_save :upload_to_s3
+
   belongs_to :project
   attr_accessor :upload
   validates :name, :upload, presence: true
+
   validates :name, uniqueness: true
   validate :upload_file_size
 
   MAX_FILESIZE = 10.megabytes
+
+  def upload_to_s3
+    s3 = Aws::S3::Resource.new
+    tenant_name = Tenant.find(Thread.current[:tenant_id]).name
+    obj = s3.bucket(ENV['AWS_S3_BUCKET']).object("#{tenant_name}/#{upload.original_filename}")
+    obj.upload_file(upload.path, acl:'public-read')
+    self.key = obj.public_url
+  end
 
 
   private
